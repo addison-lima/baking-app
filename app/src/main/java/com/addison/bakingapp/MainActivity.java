ac5afac7b2852246764dbeebc8d17139b94f9978
Addison Lima <alima@Addisons-MacBook-Air.local>
2019-09-28 15:14:31 -0400

    test10

--- app/src/main/java/com/addison/bakingapp/MainActivity.java
+++ app/src/main/java/com/addison/bakingapp/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -19,29 +21,29 @@ import com.addison.bakingapp.adapters.RecipesAdapter;
 import com.addison.bakingapp.models.Recipe;
 import com.addison.bakingapp.repository.RecipesRepository;
 import com.addison.bakingapp.viewmodels.RecipesViewModel;
+import com.addison.bakingapp.databinding.ActivityMainBinding;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
         RecipesAdapter.RecipesAdapterOnClickHandler {
 
+    private ActivityMainBinding mBinding;
     private RecipesAdapter mRecipesAdapter;
-
     private RecipesViewModel mRecipesViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
         int orientation = getResources().getConfiguration().orientation;
         int spanCount = (orientation == Configuration.ORIENTATION_LANDSCAPE) ? 2 : 1;
 
         mRecipesAdapter = new RecipesAdapter(this, this);
 
-        RecyclerView rvRecipes = findViewById(R.id.rv_recipes);
-        rvRecipes.setLayoutManager(new GridLayoutManager(this, spanCount));
-        rvRecipes.setAdapter(mRecipesAdapter);
+        mBinding.rvRecipes.setLayoutManager(new GridLayoutManager(this, spanCount));
+        mBinding.rvRecipes.setAdapter(mRecipesAdapter);
 
         mRecipesViewModel = ViewModelProviders.of(this).get(RecipesViewModel.class);
         mRecipesViewModel.getRequestState().observe(this, getRequestStateObserver());
@@ -56,15 +58,12 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.menu_refresh:
-                mRecipesViewModel.refresh();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_refresh) {
+            mRecipesViewModel.refresh();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
         }
     }
 
@@ -80,15 +79,13 @@ public class MainActivity extends AppCompatActivity implements
             @Override
             public void onChanged(RecipesRepository.RequestState requestState) {
                 if (requestState != null) {
-                    RecyclerView rvRecipes = findViewById(R.id.rv_recipes);
-                    TextView textView = findViewById(R.id.tv_request_state);
-                    textView.setText(requestState.toString());
+                    mBinding.tvRequestState.setText(requestState.toString());
                     if (requestState.equals(RecipesRepository.RequestState.SUCCESS)) {
-                        textView.setVisibility(View.INVISIBLE);
-                        rvRecipes.setVisibility(View.VISIBLE);
+                        mBinding.tvRequestState.setVisibility(View.INVISIBLE);
+                        mBinding.rvRecipes.setVisibility(View.VISIBLE);
                     } else {
-                        rvRecipes.setVisibility(View.INVISIBLE);
-                        textView.setVisibility(View.VISIBLE);
+                        mBinding.rvRecipes.setVisibility(View.INVISIBLE);
+                        mBinding.tvRequestState.setVisibility(View.VISIBLE);
                     }
                 }
             }
