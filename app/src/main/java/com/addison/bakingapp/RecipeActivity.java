32c9bba409dcd6b60f73170fbbad18748af23abb
addison.l <addison.l@samsung.com>
2019-09-30 15:35:30 -0400

    test28

--- app/src/main/java/com/addison/bakingapp/RecipeActivity.java
+++ app/src/main/java/com/addison/bakingapp/RecipeActivity.java
@@ -1,9 +1,15 @@
 package com.addison.bakingapp;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
@@ -11,6 +17,7 @@ import com.addison.bakingapp.fragments.IRecipeInfo;
 import com.addison.bakingapp.fragments.StepFragment;
 import com.addison.bakingapp.models.Recipe;
 import com.addison.bakingapp.models.Step;
+import com.addison.bakingapp.widget.BakingWidgetProvider;
 
 public class RecipeActivity extends AppCompatActivity implements IRecipeInfo {
 
@@ -38,6 +45,23 @@ public class RecipeActivity extends AppCompatActivity implements IRecipeInfo {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.recipe_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_widget) {
+            updateWidget();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public Recipe getRecipe() {
         return mRecipe;
     }
@@ -62,5 +86,13 @@ public class RecipeActivity extends AppCompatActivity implements IRecipeInfo {
             intent.putExtra("recipe", mRecipe);
             startActivity(intent);
         }
+    }
+
+    private void updateWidget() {
+        ComponentName provider = new ComponentName(this, BakingWidgetProvider.class);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] ids = appWidgetManager.getAppWidgetIds(provider);
+        BakingWidgetProvider bakingWidgetProvider = new BakingWidgetProvider();
+        bakingWidgetProvider.onUpdate(this, appWidgetManager, ids);
     }
 }
---
+++ app/src/main/java/com/addison/bakingapp/widget/BakingWidgetProvider.java
@@ -0,0 +1,13 @@
+package com.addison.bakingapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class BakingWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
